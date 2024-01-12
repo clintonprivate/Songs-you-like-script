79cc98fee72d1b32a6af425ecc14d39d9460f71f
clintonprivate <152466284+clintonprivate@users.noreply.github.com>
2024-01-12 04:13:21 +0100

    Omit bass clef function

--- songsyoulike.py
+++ songsyoulike.py
@@ -1,12 +1,19 @@
 import os
 import random
+from music21 import *
 
 songsDirectory = os.path.join(os.getcwd(), "songsyoulike")
 snippetCriteria = "| D major | Treble clef | Single note | Intervals of 1 | Sixteenth note |"
 
 def pickRandomSong():
     files = [f for f in os.listdir(songsDirectory) if os.path.isfile(os.path.join(songsDirectory, f))]
-    random_file = random.choice(files)
-    print(random_file)
+    randomFile = random.choice(files)
+    filePath = "songsyoulike/" + randomFile
+    return filePath
 
-pickRandomSong()
+def omitBassClef(inputFile):
+    score = converter.parse(inputFile)
+    score.parts[1].activeSite.remove(score.parts[1])
+    score.write('musicxml', fp="output.xml", xml_declaration=True, omit_encoding=False)
+
+omitBassClef("songsyoulike/Shining Star.mid")
